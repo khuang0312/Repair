983421ef014f9f7627d99dd9007b2bcad3c32c2e
Kevin Huang <huangk11@uci.edu>
2020-02-01 21:05:04 -0800

    Added the clear board script and started working on the "artificial intelligence"

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    //walk forward one unit at a time
+
+
+    //walk backward if its at a pit...
+
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Movement()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ClearBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearBoard : MonoBehaviour
+{
+    //purges anything that is a block from the game...
+
+    void clearBoard()
+    {
+        GameObject[] blocks = GameObject.FindGameObjectsWithTag("Blocks");
+
+        foreach (GameObject b in blocks)
+        {
+            Destroy(b);
+        }
+
+    }
+}
